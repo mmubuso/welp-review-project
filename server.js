03c7271b8f6e9fba21fe7428e1f898ec491f5d26
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-27 10:31:13 -0400

    added path for index page

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ const { templateRouter } = require('./controllers/template.js')
 /* Step 3.a
  * ...to parse the body of the HTTP requests from a URL encoded string 
  */
+
+ //creates
+app.get('/', (req,res) => {
+    res.send('Home Page')
+})
 app.use(express.urlencoded({extended: true}))
 
 /* Step 3.b 
